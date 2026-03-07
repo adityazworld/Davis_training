d3b27fd3abb4e3b8a3e3c3e55a974b0a92083e35
Aditya <kushwahaaditya977@gmail.com>
2026-03-07 11:14:38 +0530

    Update areaparameter.py

--- Class_work/selection_Statement/areaparameter.py
+++ Class_work/selection_Statement/areaparameter.py
@@ -1,27 +1,31 @@
 #wap to find the area and perimeter of rectangle and validate the data wherever required
-# taking input from the user
+# taking input  length from the user
 
 length = float(input("Enter the length of the rectangle: "))
 # validating the length
 if length < 0:
     exit("Error: Length cannot be negative.")
+# taking input breadth from the user
 
-width = float(input("Enter the width of the rectangle: "))
-# validating the width
-if width < 0:
-    exit("Error: Width cannot be negative.")
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+# validating the breadth
+if breadth < 0:
+    exit("Error: Breadth cannot be negative.")
 
 # calculating area and perimeter
-area = length * width
-perimeter = 2 * (length + width)
+area = length * breadth
+perimeter = 2 * (length + breadth)
 
 # displaying the results
+print("--------------------------------")
 print("The area of the rectangle is:", area)
 print("The perimeter of the rectangle is:", perimeter)
 
 
 '''Output:
 Enter the length of the rectangle: 5
-Enter the width of the rectangle: 3
+Enter the breadth of the rectangle: 3
 The area of the rectangle is: 15.0
 The perimeter of the rectangle is: 16.0
+
