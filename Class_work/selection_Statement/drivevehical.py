82aec73c01c80d116eda14a2aa5aa2999ea6b4e6
Aditya <kushwahaaditya977@gmail.com>
2026-03-07 19:43:53 +0530

    Update drivevehical.py in selection_Statement folder

---
+++ Class_work/selection_Statement/drivevehical.py
@@ -0,0 +1,15 @@
+# Eligible to Drive Gear Vehicle
+# age should be greater than 18
+# input age from user
+age = int(input("Enter your age: "))
+# check if age is greater than 18
+if age > 18:
+    print("You are eligible to drive a gear vehicle.")      
+else:
+    print("You are not eligible to drive a gear vehicle.")
+
+ '''Output:
+Enter your age: 20
+You are eligible to drive a gear vehicle.
+
+
