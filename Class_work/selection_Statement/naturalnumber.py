2876072e667f5e8e134e1b4968f179b4988876ea
Aditya <kushwahaaditya977@gmail.com>
2026-03-07 10:35:38 +0530

    Add naturalnumber.py to selection_Statement

---
+++ Class_work/selection_Statement/naturalnumber.py
@@ -0,0 +1,15 @@
+# check the number is natural number or not
+# input number from user
+number = int(input("Enter a number: "))
+# check if number is greater than 0
+if number > 0:
+
+    print(number, "is a natural number.")
+else:
+    print(number, "is not a natural number.")
+
+    '''Output:
+    Enter a number: 5
+    5 is a natural number.
+    Enter a number: -3
+    -3 is not a natural number.'''
