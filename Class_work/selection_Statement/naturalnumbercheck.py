731a6fffca7850a3b844c45877a4be5ae5f0c60f
Aditya <kushwahaaditya977@gmail.com>
2026-03-07 10:30:24 +0530

    Add naturalnumbercheck.py to selection_Statement

---
+++ Class_work/selection_Statement/naturalnumbercheck.py
@@ -0,0 +1,17 @@
+# check thre digit number is natural number or not
+# taking input from user
+
+num = int(input("Enter a three digit number: "))
+# check if the number is a three digit number
+if num >= 100 and num <= 999:   
+    print(num, "is a three digit number.")
+else:
+
+    print(num, "is not a three digit number.")
+
+    '''Output:
+    Enter a three digit number: 123
+    123 is a three digit number.
+    Enter a three digit number: 99
+    99 is not a three digit number.'''
+    
