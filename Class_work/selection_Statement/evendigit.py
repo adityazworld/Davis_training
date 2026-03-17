5000ba371e2329dd3eda87737433b08dcc4ef2b1
Aditya <kushwahaaditya977@gmail.com>
2026-03-17 10:13:00 +0530

    Add Class_work/function and evendigit.py

---
+++ Class_work/selection_Statement/evendigit.py
@@ -0,0 +1,13 @@
+## program to display even factors of a number
+
+
+num = int(input("Enter a number: "))
+print("Even factors of", num, "are:")
+
+    for i in range(1, num+1):
+        if num % i == 0 and i % 2 == 0:
+            print("The even factor is :" i)
+
+'''Output:  
+Enter a number: 12
+Even factors of 12 are: 2 4 6 12
